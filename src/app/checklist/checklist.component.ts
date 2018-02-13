5e3f9f60e0d6d5240dea3de7df4c2e6b742ffe6d
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2018-02-13 09:00:41 +0100

    @Input()

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-checklist',
@@ -10,7 +10,7 @@ export class ChecklistComponent {
   constructor() { }
 
   newTask: string;
-  tasksList: Array<string> = [];
+  @Input() tasksList: Array<string> = [];
   tasksDone: Array<string> = [];
 
   add(){
