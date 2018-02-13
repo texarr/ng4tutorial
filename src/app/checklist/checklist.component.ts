72fb4ccf7761915010d3564ee3c4a958f26ae5b4
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2018-02-13 08:51:18 +0100

    #34 Add task lists

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -11,10 +11,19 @@ export class ChecklistComponent {
 
   newTask: string;
   tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
 
   add(){
     this.tasksList.push(this.newTask);
     this.newTask = '';
-    console.log(this.tasksList);
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(e => e!== task);
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
   }
 }
