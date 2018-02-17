b4548681c62efb112f7e59b585c71648a6b8b555
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2018-02-17 22:25:24 +0100

    #49 Use service in components as multiple instances

--- src/app/klikacz1/klikacz1.component.ts
+++ src/app/klikacz1/klikacz1.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ClickService } from '../services/click.service';
 
 @Component({
   selector: 'app-klikacz1',
   templateUrl: './klikacz1.component.html',
-  styleUrls: ['./klikacz1.component.css']
+  styleUrls: ['./klikacz1.component.css'],
+  providers: [ClickService]
 })
 export class Klikacz1Component implements OnInit {
   clicks: number = 0;
 
-  constructor() { }
+  constructor(private clickService: ClickService) { }
 
   ngOnInit() {
   }
 
   add() {
     this.clicks++;
+    this.clickService.addClicks();
   }
 
 }
--- src/app/klikacz2/klikacz2.component.ts
+++ src/app/klikacz2/klikacz2.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ClickService } from "../services/click.service";
 
 @Component({
   selector: 'app-klikacz2',
   templateUrl: './klikacz2.component.html',
-  styleUrls: ['./klikacz2.component.css']
+  styleUrls: ['./klikacz2.component.css'],
+  providers: [ClickService]
 })
 export class Klikacz2Component implements OnInit {
   clicks: number = 0;
 
-  constructor() { }
+  constructor(private clickService: ClickService) { }
 
   ngOnInit() {
   }
 
   add() {
     this.clicks++;
+    this.clickService.addClicks();
   }
 
 }
