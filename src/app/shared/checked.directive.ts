5bdbe9b2ac99cdbd6a0713604e40eecaf0334089
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2018-02-25 13:26:07 +0100

    #58 Add directive with renderer2 class and ElementRef

--- src/app/shared/checked.directive.ts
+++ src/app/shared/checked.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appChecked]'
 })
-export class CheckedDirective {
+export class CheckedDirective implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
+  ngOnInit(): void {
+    let li = this.el.nativeElement;
+    this.renderer.setStyle(li, 'list-style-image', 'url(/assets/checked.png)');
+    this.renderer.setStyle(li, 'border', '1px solid red');
+  }
 }
