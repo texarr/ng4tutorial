3e3dad76cd828dfa7a2edd4f01a6d76ac94f425a
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2018-02-27 22:40:28 +0100

    Add pipe transformation function and return value

--- src/app/shared/transform-task.pipe.ts
+++ src/app/shared/transform-task.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TransformTaskPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return value;
+  transform(value: string, args?: any): any {
+    return value.charAt(0).toUpperCase() + value.slice(1);
   }
 
 }
