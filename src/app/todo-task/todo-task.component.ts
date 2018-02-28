df0aad15e4649decdf6424f23071038c615cf457
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2018-02-28 22:19:24 +0100

    #66 Sort function optimizing, set pure mode true in pipe

--- src/app/todo-task/todo-task.component.ts
+++ src/app/todo-task/todo-task.component.ts
@@ -18,7 +18,7 @@ export class TodoTaskComponent {
 
   constructor(private tasksService: TasksService) {
     this.tasksService.getTasksListObs().subscribe((tasks: Array<Task>) => {
-      this.tasksList = tasks;
+      this.tasksList = tasks.slice();
     });
   }
 
