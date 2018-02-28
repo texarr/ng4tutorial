dbb3879edabe2fd4a6d7c344e37c0fc92cb540b1
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2018-02-28 22:04:40 +0100

    #64 Add args to custom pipes

--- src/app/shared/transform-task.pipe.ts
+++ src/app/shared/transform-task.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TransformTaskPipe implements PipeTransform {
 
-  transform(value: string, args?: any): any {
-    return value.charAt(0).toUpperCase() + value.slice(1);
+  transform(value: string, args: string = ''): any {
+    return value.charAt(0).toUpperCase() + value.slice(1) + args;
   }
 
 }
